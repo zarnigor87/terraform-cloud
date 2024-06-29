10088a14ac5628c9cd0ff2c1218e74d2088a9124
Zara <153763391+zarnigor87@users.noreply.github.com>
2024-06-29 16:31:37 -0500

    added db instance

--- rds/main.tf
+++ rds/main.tf
@@ -14,7 +14,18 @@ resource "aws_db_subnet_group" "cloud" {
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
 
-
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.cloud.name           # Create in my VPC
+}
 
 
 
